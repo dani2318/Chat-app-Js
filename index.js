eb8cb4344ac76c29c12b2af78773ded67228dc43
dani2318 <70331791+dani2318@users.noreply.github.com>
2021-04-30 11:29:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,30 +5,22 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const utenti=[""]
+
 app.use(express.static(__dirname + '/public'));
 app.get('/public', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/style.css');
 });
 
 io.on('connection', (socket) => {
 
   socket.on('connected', (msg) => {
-    console.log(msg)
-    utenti.push(msg)
-    console.log(utenti[utenti.length])
-    var res = [`${msg} si è connesso alla chat`,utenti[utenti.length]]
+    var res = `${msg} si è connesso alla chat`
     io.emit('response', res);
   })
 
   socket.on('message', (msg) => {
     console.log(msg[0] + " , " + msg[1])
-    var messaggio = msg[1] + ": " + msg[0]
-    io.emit('chat message', messaggio);
-  });
-
-  socket.on('command', (msg) => {
-    console.log(msg[0] + " , " + msg[1])
-    console.log(utenti.length)
     var messaggio = msg[1] + ": " + msg[0]
     io.emit('chat message', messaggio);
   });
